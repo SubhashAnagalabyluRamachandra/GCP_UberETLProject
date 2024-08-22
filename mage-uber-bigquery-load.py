b79472f02273d1ac3df94b7cb0144d06948c4b78
Subhash Anagalabylu Ramachandra <123510516+SubhashAnagalabyluRamachandra@users.noreply.github.com>
2024-08-22 04:26:41 -0500

    Update mage-uber-bigquery-load.py

--- mage-uber-bigquery-load.py
+++ mage-uber-bigquery-load.py
@@ -10,12 +10,7 @@ if 'data_exporter' not in globals():
 
 @data_exporter
 def export_data_to_big_query(data, **kwargs) -> None:
-    """
-    Template for exporting data to a BigQuery warehouse.
-    Specify your configuration settings in 'io_config.yaml'.
-
-    Docs: https://docs.mage.ai/design/data-loading#bigquery
-    """
+  
     config_path = path.join(get_repo_path(), 'io_config.yaml')
     config_profile = 'default'
 
